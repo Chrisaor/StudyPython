acc93a2484ebd9ba2f9de5db29b6790b33a6f7d9
Chrisaor <pchpch0070@gmail.com>
2017-12-21 00:50:15 +0900

    add clean code

--- Programmers/Level 1/3.SumDigit.py
+++ Programmers/Level 1/3.SumDigit.py
@@ -5,5 +5,15 @@ def sum_digit(number):
 		number //= 10
 	return SumDigit
 
-# # 아래는 테스트로 출력해 보기 위한 코드입니다.
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
 print("결과 : {}".format(sum_digit(123)));
+
+'''<Clean code>
+def sum_digit(number):
+    if number < 10:
+        return number;
+    return (number % 10) + sum_digit(number // 10) 
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print("결과 : {}".format(sum_digit(123)));'''
+
